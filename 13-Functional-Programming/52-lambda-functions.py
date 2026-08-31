f51bad18ccef9afbccee42b9a2da8ffcde6f0917
Dev Goyal <devgoyalg2346@gmail.com>
2026-08-31 13:15:11 +0530

    52-lambda-functions.py added

---
+++ 13-Functional-Programming/52-lambda-functions.py
@@ -0,0 +1,8 @@
+# lambda fun - it is a small anonymous func without a name. 
+# it is defind using the lambda keyword
+
+double = lambda x : x*2
+print(double(7)) 
+
+avg = lambda x,y: (x+y)/2
+print(avg(4,5))
