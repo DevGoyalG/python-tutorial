0d073ff35424dc95783b6f22e1b23b9575f7f3ca
Dev Goyal <devgoyalg2346@gmail.com>
2026-08-31 21:55:30 +0530

    57-classes-and-objects.py added

--- 14-OOP/57-classes-and-objects.py
+++ 14-OOP/57-classes-and-objects.py
@@ -1,11 +1,18 @@
-# Classes and Objects in Python #
+# Classes and Objects in Python
 
-class Person:
+# class - it is a blueprint or a template for creating objects, providing initial values for state (member var or attributes) 
+# and implementation of behavior (member func or methods)
+
+# object - it is the instance of the class used to access the properties of the class
+
+# self - self parameter is a reference to the current instance of the class and i used to access variables that belongs to the class
+
+class Person:                   # class
     name = "Dev Goyal"
     occupation = "Student"
     networth = 10
 
-a = Person()
+a = Person()                    # object
 print(a.name)
 
 
