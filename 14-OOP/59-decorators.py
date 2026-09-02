bd2354393c6cd2b6c81bb22a82cf508545172007
Dev Goyal <devgoyalg2346@gmail.com>
2026-09-02 21:11:03 +0530

    59-decorators.py added

--- 14-OOP/59-decorators.py
+++ 14-OOP/59-decorators.py
@@ -1,12 +1,23 @@
 # Decorators in Python #
 
+# decorator - are a powerful and versatile tool that allow you to modify the behavior of func and methods.
 
+def greet(fx):
+    def mfx(*args, **kwargs):
+        print("Good Morning")
+        fx(*args, **kwargs)
+        print("Thanks for using this function.")
+    return mfx
 
+@greet
+def hello():
+    print("Hello World")
 
+@greet
+def add(a,b):
+    print(a+b)
 
-
-
-
-
-
-
+# greet(hello)()    # basic way
+hello()             # best way
+# greet(add)(1,2)
+add(1,2)
