178a1cb14460fcfa76ccb262302b03f898065aeb
Dev Goyal <devgoyalg2346@gmail.com>
2026-09-13 20:49:53 +0530

    98-multiprocessing.py added

---
+++ 18-Concurrency/98-multiprocessing.py
@@ -0,0 +1,7 @@
+# multiprocessing in python - a technique that allows a single process to spawn and run multiple threads concurrently.
+# A thread is the smallest unit of execution inside a program.
+# a technique that allows a single process to spawn and run multiple threads concurrently. 
+# A thread is the smallest unit of execution inside a program. In Python, multithreading allows a program to split a main task 
+# into several smaller sub-tasks that run in an overlapping manner, sharing the same memory space, code, and variables
+
+
